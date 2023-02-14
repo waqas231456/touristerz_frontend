6fbc0ff704ff996612bd868140fd0dd4de971d16
waqas231456 <waqaskhalid2314@gmail.com>
2023-02-14 15:10:12 +0500

    admin Dashboard

--- components/AdminDashboard/dash.jsx
+++ components/AdminDashboard/dash.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import Layout from "../Layout";
 import Avatar from "../Sidebar/avator";
 import Style from "./style.module.css";
-function Dashboard() {
+import axios from "axios";
+function AdminDashboard() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // console.log("i amahere")
+
+    const getdata = async () => {
+      try {
+        axios.get(`http://localhost:5000/api/dash/data`).then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getdata();
+  }, []);
   return (
     <Layout>
       <div className={Style.dashboard}>
@@ -14,7 +34,7 @@ function Dashboard() {
           <div className={Style.flex2}>
             <div className={Style.Card1} style={{ textAlign: "left" }}>
               {/* <FontAwesomeIcon icon="faYoutube" style={{float:"right", marginRight:"9%", paddingTop:"3%"}}/> */}
-              <h1 className={Style.text}>Number</h1>
+              <h1 className={Style.text}>{data.totalUsers}</h1>
               <h3 className={Style.text}>Agencey</h3>
              
             </div>
@@ -46,4 +66,4 @@ function Dashboard() {
   );
 }
 
-export default Dashboard;
+export default AdminDashboard;
